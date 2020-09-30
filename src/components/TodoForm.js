33036bc8cf94697cb5355f7bfb8b7db2c8b70f41
mccans32 <smccann1998@gmail.com>
2020-09-30 23:33:28 +0100

    Added material ui css framework

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,18 +1,35 @@
 import React from 'react';
-import './TodoForm';
+import './TodoForm.css';
+import { TextField } from '@material-ui/core';
 
 class TodoForm extends React.Component {
 
-    constructor(props) {
+    constructor() {
         super();
-        this.state = {};
+        this.handleChange = this.handleChange.bind(this)
+        this.state = {
+            formData: ""
+        };
+    }
 
+    handleChange(event) {
+        this.setState({
+            formData: event.target.value,
+        })
     }
 
     render() {
         return (
             <div className="todoForm">
-                <form>This is the Todo Form</form>
+                <p className="todoHeader">Enter a new todo</p>
+                <TextField
+                    className="todoTextField"
+                    multiline="true"
+                    placeholder="Empty"
+                    value={this.state.formData}
+                    onChange={this.handleChange}
+                    >
+                </TextField>
             </div>
         )
     }
